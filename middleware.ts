4858d21db614dab806bdd81b7c6d354d60ed8932
duc30012001 <nguyenvietduc3001@gmail.com>
2025-07-16 22:39:16 +0700

    chore: temporary no check permission

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { AppRoute, SIDEBAR_ITEMS } from '@/constants/sidebar';
+import { AppRoute } from '@/constants/sidebar';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import { getToken } from './lib/auth';
@@ -17,6 +17,7 @@ export async function middleware(req: NextRequest) {
 
     // 2. Check for a valid session-jwt
     const token = await getToken(req);
+    console.log('token:', token);
 
     const isOnSigninPage = pathname === AppRoute.Signin;
 
@@ -34,15 +35,19 @@ export async function middleware(req: NextRequest) {
         return NextResponse.redirect(dashUrl);
     }
 
-    const currentRoute = SIDEBAR_ITEMS.find((item) => item.href === pathname);
-    if (token && currentRoute?.role) {
-        const userRole = token.role;
-        if (userRole !== currentRoute.role) {
-            const forbiddenUrl = req.nextUrl.clone();
-            forbiddenUrl.pathname = AppRoute.Forbidden;
-            return NextResponse.redirect(forbiddenUrl);
-        }
-    }
+    // const tokenPayload = getDataFromToken(token?.accessToken)
+
+    // const currentRoute = SIDEBAR_ITEMS.find((item) => item.href === pathname);
+    // if (token && currentRoute?.role) {
+    //     const userRole = tokenPayload?.role;
+    //     console.log('userRole:', userRole);
+    //     console.log('currentRoute:', currentRoute);
+    //     if (userRole !== currentRoute.role) {
+    //         const forbiddenUrl = req.nextUrl.clone();
+    //         forbiddenUrl.pathname = AppRoute.Forbidden;
+    //         return NextResponse.redirect(forbiddenUrl);
+    //     }
+    // }
 
     // 4. Otherwise allow
     return NextResponse.next();
