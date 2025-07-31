c936c5540ac80117501fbdc5ffa5f99c7b9414ab
duc30012001 <nguyenvietduc3001@gmail.com>
2025-07-31 21:21:48 +0700

    ci: update flow

--- next.config.mjs
+++ next.config.mjs
@@ -13,12 +13,15 @@ const nextConfig = {
         ];
     },
     async rewrites() {
-        return [
-            {
-                source: '/api/v1/:path*',
-                destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
-            },
-        ];
+        if (process.env.NEXT_PUBLIC_API_BASE_URL) {
+            return [
+                {
+                    source: '/api/v1/:path*',
+                    destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+                },
+            ];
+        }
+        return undefined;
     },
     images: {
         remotePatterns: [
