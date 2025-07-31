e81540dc0c04c37fec39a7ba9b6e4a764a2b4df2
duc30012001 <nguyenvietduc3001@gmail.com>
2025-07-31 22:37:45 +0700

    chore: check show support button

--- components/ui/button/support-button.tsx
+++ components/ui/button/support-button.tsx
@@ -4,14 +4,18 @@ import { CircleQuestionMark } from 'lucide-react';
 type Props = {};
 
 function SupportButton({}: Props) {
-    return (
-        <FloatButton
-            icon={<CircleQuestionMark />}
-            onClick={() => {
-                window.open(process.env.NEXT_PUBLIC_SUPPORT_LINK, '_blank');
-            }}
-        />
-    );
+    const supportUrl = process.env.NEXT_PUBLIC_SUPPORT_LINK;
+    if (supportUrl) {
+        return (
+            <FloatButton
+                icon={<CircleQuestionMark />}
+                onClick={() => {
+                    window.open(process.env.NEXT_PUBLIC_SUPPORT_LINK, '_blank');
+                }}
+            />
+        );
+    }
+    return null;
 }
 
 export default SupportButton;
